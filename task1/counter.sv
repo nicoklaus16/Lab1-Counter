4071a5e1551842ed28625d5ff6c69c184de98a8b
Nico <n.sanchez.f10@gmail.com>
2023-06-06 17:07:50 +0100

    counter source file

--- task1/counter.sv
+++ task1/counter.sv
@@ -5,7 +5,7 @@ module counter #(
     input logic                 clk,        //clock
     input logic                 rst,        //reset
     input logic                 en,         //counter enable
-    output logic [WIDTH-1:0]    count, //counter output
+    output logic [WIDTH-1:0]    count //counter output
 );
 
 always_ff @(posedge clk)

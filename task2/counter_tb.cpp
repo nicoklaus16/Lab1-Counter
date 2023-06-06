adb2654f50d64f50f673fa83e155879fabc5b4bc
Nico <n.sanchez.f10@gmail.com>
2023-06-06 19:59:02 +0100

    count up and down mode, draw dots instead of count value

--- task2/counter_tb.cpp
+++ task2/counter_tb.cpp
@@ -26,7 +26,7 @@ int main(int argc, char **argv, char **env) {
     top->en = 0;
 
     //run simulation for 150 clock cycles
-    for (i=0; i<300; i++){
+    for (i=0; i<3000; i++){
 
         //dump variables into VCD file and toggle clock
         for (clk=0; clk<2; clk++) {
@@ -36,14 +36,16 @@ int main(int argc, char **argv, char **env) {
         }
 
         // ++++ Send count value to Vbuddy
-        vbdHex(4, (int(top->count) >> 16) & 0xF);
+        /*vbdHex(4, (int(top->count) >> 16) & 0xF);
         vbdHex(3, (int(top->count) >> 8) & 0xF);
         vbdHex(2, (int(top->count) >> 4) & 0xF);
-        vbdHex(1, int(top->count) & 0xF);
+        vbdHex(1, int(top->count) & 0xF);*/         //comment out the 7-segment display mode
+
+        vbdPlot(int(top->count), 0, 255);           //use Plot function instead
         // ---- end of Vbuddy output section
 
         top->rst = (i<2) | (i==15);
-        top->en = (i>4);
+        top->en = vbdFlag();
         if (Verilated::gotFinish())  exit(0);
     }
     vbdClose();     // ++++

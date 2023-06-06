4885ab90ad3a9058814a9dcd838d2dc3ab289ad9
Nico <n.sanchez.f10@gmail.com>
2023-06-06 18:35:04 +0100

    added Vbuddy features to test bench

--- task2/counter_tb.cpp
+++ task2/counter_tb.cpp
@@ -1,6 +1,7 @@
 #include "Vcounter.h"
 #include "verilated.h"
 #include "verilated_vcd_c.h"
+#include "vbuddy.cpp"
 
 int main(int argc, char **argv, char **env) {
     int i;
@@ -14,6 +15,10 @@ int main(int argc, char **argv, char **env) {
     VerilatedVcdC* tfp = new VerilatedVcdC;
     top->trace (tfp, 99);
     tfp->open ("counter.vcd");
+
+    //init Vbuddy
+    if (vbdOpen()!=1) return(-1);
+    vbdHeader("Lab 1: Counter");
 
     // initialize simulation inputs
     top->clk = 1;
@@ -29,10 +34,19 @@ int main(int argc, char **argv, char **env) {
             top->clk = !top->clk;
             top->eval ();
         }
+
+        // ++++ Send count value to Vbuddy
+        vbdHex(4, (int(top->count) >> 16) & 0xF);
+        vbdHex(3, (int(top->count) >> 8) & 0xF);
+        vbdHex(2, (int(top->count) >> 4) & 0xF);
+        vbdHex(1, int(top->count) & 0xF);
+        // ---- end of Vbuddy output section
+
         top->rst = (i<2) | (i==15);
         top->en = (i>4);
         if (Verilated::gotFinish())  exit(0);
     }
+    vbdClose();     // ++++
     tfp->close();
     exit(0);
 }

6ca679e9734f8e449b90353f26856d619f3a1667
Nico <n.sanchez.f10@gmail.com>
2023-06-07 21:02:29 +0100

    task 3 solution

---
+++ task3/counter_tb.cpp
@@ -0,0 +1,59 @@
+#include "Vcounter.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "vbuddy.cpp"
+
+int main(int argc, char **argv, char **env) {
+    int i;
+    int clk;
+    int parameter;
+
+    Verilated::commandArgs(argc, argv);
+    //init top verilog instance
+    Vcounter* top = new Vcounter;
+    //init trace dump
+    Verilated::traceEverOn (true);
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    top->trace (tfp, 99);
+    tfp->open ("counter.vcd");
+
+    //init Vbuddy
+    if (vbdOpen()!=1) return(-1);
+    vbdHeader("Lab 1: Counter");
+
+    // initialize simulation inputs
+    top->clk = 1;
+    top->rst = 1;
+    top->ld = 0;
+    top->v = 0;
+
+    vbdSetMode(1);
+
+    //run simulation for 150 clock cycles
+    for (i=0; i<3000; i++){
+
+        //dump variables into VCD file and toggle clock
+        for (clk=0; clk<2; clk++) {
+            tfp->dump (2*i+clk);
+            top->clk = !top->clk;
+            top->eval ();
+        }
+
+        // ++++ Send count value to Vbuddy
+        vbdHex(4, (int(top->count) >> 16) & 0xF);
+        vbdHex(3, (int(top->count) >> 8) & 0xF);
+        vbdHex(2, (int(top->count) >> 4) & 0xF);
+        vbdHex(1, int(top->count) & 0xF);
+
+        //vbdPlot(int(top->count), 0, 255);           //Plot dots
+        // ---- end of Vbuddy output section
+
+        top->rst = (i<2);
+        top->ld = vbdFlag();
+        top->v = vbdValue();
+        if (Verilated::gotFinish())  exit(0);
+    }
+    vbdClose();     // ++++
+    tfp->close();
+    exit(0);
+}
